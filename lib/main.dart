8953665dfffa131e6c518f5b9929c093f446f0f2
Mansi <magdyashraf1811@gmail.com>
2026-04-06 18:23:38 +0200

    + initialize project +added empty home screen

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:islami_app/home/home_screen.dart';
+import 'package:islami_app/utils/app_routes.dart';
+
+void main(){
+  runApp(Myapp());
+}
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppRoutes.homeRouteName ,
+      routes: {
+        AppRoutes.homeRouteName: (context) =>  HomeScreen() ,
+      },
+    );
+  }
+}
