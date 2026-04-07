41cec999e1b9282dbae4f5a1c6c70a1b61dccb04
Mansi <magdyashraf1811@gmail.com>
2026-04-07 20:15:48 +0200

    + initialize project +added empty home screen +added splashscreen +added BottomNavigationBar +added ui for quran tab

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app/home/tabs/Quran/quran_tab.dart';
+import 'package:islami_app/home/tabs/hadeth_tab/hadeth_tab.dart';
+import 'package:islami_app/home/tabs/radio%20tab/radio_tab.dart';
+import 'package:islami_app/home/tabs/sebha%20tab/sebha_tab.dart';
+import 'package:islami_app/home/tabs/time%20tab/time_tab.dart';
 import 'package:islami_app/utils/app_color.dart';
 import 'package:islami_app/utils/images.dart';
 
@@ -13,57 +18,84 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int selectedIndex = 0;
+  List<Widget> tabsList = [
+    QuranTab(),
+    HadethTab(),
+    SebhaTab(),
+    RadioTab(),
+    TimeTab(),
+  ];
+  List<String> backGroundImage = [
+    AppAssets.quranBg,
+    AppAssets.hadethBg,
+    AppAssets.sabhaBg,
+    AppAssets.radioBg,
+    AppAssets.timeBg,
+  ];
 
   @override
   Widget build(BuildContext context) {
+    var height =MediaQuery.of(context).size.height ;
+
     return Stack(
       children: [
         Image.asset(
-          AppAssets.quranBg,
+          backGroundImage[selectedIndex],
           width: double.infinity,
+          height: double.infinity,
           fit: BoxFit.fill,
         ),
-        Scaffold(
-          backgroundColor: Colors.transparent,
-          bottomNavigationBar: Theme(
-            data: Theme.of(
-              context,
-            ).copyWith(canvasColor: AppColor.primaryColor),
-            child: BottomNavigationBar(
-              currentIndex: selectedIndex,
-              onTap: (index) {
-                selectedIndex = index;
-                setState(() {});
-              },
-
-              backgroundColor: AppColor.primaryColor,
-              type: BottomNavigationBarType.fixed,
-              items: [
-                builtBottomNavBar(
-                  iconName: AppAssets.iconQuranBg,
-                  label: 'Quran',
-                  index: 0,
-                ),
-                builtBottomNavBar(
-                  iconName: AppAssets.hadethIcon,
-                  label: 'Hadeth',
-                  index: 1,
-                ),
-                builtBottomNavBar(
-                  iconName: AppAssets.sebhaIcon,
-                  label: 'Sebha',
-                  index: 2,
-                ),
-                builtBottomNavBar(
-                  iconName: AppAssets.radioIcon,
-                  label: 'Radio',
-                  index: 3,
-                ),
-                builtBottomNavBar(
-                  iconName: AppAssets.timeIcon,
-                  label: 'Time',
-                  index: 4,
-                ),
+        SafeArea(
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            bottomNavigationBar: Theme(
+              data: Theme.of(
+                context,
+              ).copyWith(canvasColor: AppColor.primaryColor),
+              child: BottomNavigationBar(
+                currentIndex: selectedIndex,
+                onTap: (index) {
+                  selectedIndex = index;
+                  setState(() {});
+                },
+          
+                backgroundColor: AppColor.primaryColor,
+                type: BottomNavigationBarType.fixed,
+                items: [
+                  builtBottomNavBar(
+                    iconName: AppAssets.iconQuranBg,
+                    label: 'Quran',
+                    index: 0,
+                  ),
+                  builtBottomNavBar(
+                    iconName: AppAssets.hadethIcon,
+                    label: 'Hadeth',
+                    index: 1,
+                  ),
+                  builtBottomNavBar(
+                    iconName: AppAssets.sebhaIcon,
+                    label: 'Sebha',
+                    index: 2,
+                  ),
+                  builtBottomNavBar(
+                    iconName: AppAssets.radioIcon,
+                    label: 'Radio',
+                    index: 3,
+                  ),
+                  builtBottomNavBar(
+                    iconName: AppAssets.timeIcon,
+                    label: 'Time',
+                    index: 4,
+                  ),
+                ],
+              ),
+            ),
+            body: Column(
+              spacing:height*0.01 ,
+          
+              children: [
+                Image.asset(AppAssets.logo),
+                Expanded(child: tabsList[selectedIndex]),
               ],
             ),
           ),
@@ -78,15 +110,17 @@ class _HomeScreenState extends State<HomeScreen> {
     required int index,
   }) {
     return BottomNavigationBarItem(
-      icon:selectedIndex==index?
-      Container(
-        padding: EdgeInsets.symmetric(horizontal: 20,vertical: 6),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(66),
-          color: AppColor.blackbgColor
-        ),
-        child: ImageIcon(AssetImage(iconName)),
-      ):ImageIcon(AssetImage(iconName)) ,  label: label
+      icon: selectedIndex == index
+          ? Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 6),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(66),
+                color: AppColor.blackbgColor,
+              ),
+              child: ImageIcon(AssetImage(iconName)),
+            )
+          : ImageIcon(AssetImage(iconName)),
+      label: label,
     );
   }
 }
---
+++ lib/home/tabs/Quran/widget/sura_item_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:islami_app/models/quran_resources.dart';
+import 'package:islami_app/utils/app_styles.dart';
+import 'package:islami_app/utils/images.dart';
+
+class SuraItemWidget extends StatelessWidget {
+  final int  index;
+  const SuraItemWidget({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+
+    return Row(
+      spacing: width*0.06,
+      children: [
+        Stack(
+          alignment: Alignment.center,
+          children: [
+            Image.asset(AppAssets.suraNum),
+            Text('${index+1}',style: AppStyles.bold16White,)
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(QuranResources.englishNames[index],style: AppStyles.bold20white,),
+            Text('${QuranResources.versesCount[index]} Verses',style: AppStyles.bold14white,)
+          ],
+        ),
+        Spacer(),
+        Text(QuranResources.arabicNames[index],style: AppStyles.bold20white,)
+      ],
+    );
+  }
+}
