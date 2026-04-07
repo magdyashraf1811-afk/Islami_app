54e24d3080162b4a1aac23e6def1e779352e7cbb
Mansi <magdyashraf1811@gmail.com>
2026-04-07 17:13:47 +0200

    + initialize project +added empty home screen +added splashscreen +added BottomNavigationBar

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,12 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app/utils/app_color.dart';
+import 'package:islami_app/utils/images.dart';
 
-class HomeScreen extends StatelessWidget {
-  static const String routeName ='home_screen' ;
+class HomeScreen extends StatefulWidget {
+  static const String routeName = 'home_screen';
 
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Stack(
+      children: [
+        Image.asset(
+          AppAssets.quranBg,
+          width: double.infinity,
+          fit: BoxFit.fill,
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          bottomNavigationBar: Theme(
+            data: Theme.of(
+              context,
+            ).copyWith(canvasColor: AppColor.primaryColor),
+            child: BottomNavigationBar(
+              currentIndex: selectedIndex,
+              onTap: (index) {
+                selectedIndex = index;
+                setState(() {});
+              },
+
+              backgroundColor: AppColor.primaryColor,
+              type: BottomNavigationBarType.fixed,
+              items: [
+                builtBottomNavBar(
+                  iconName: AppAssets.iconQuranBg,
+                  label: 'Quran',
+                  index: 0,
+                ),
+                builtBottomNavBar(
+                  iconName: AppAssets.hadethIcon,
+                  label: 'Hadeth',
+                  index: 1,
+                ),
+                builtBottomNavBar(
+                  iconName: AppAssets.sebhaIcon,
+                  label: 'Sebha',
+                  index: 2,
+                ),
+                builtBottomNavBar(
+                  iconName: AppAssets.radioIcon,
+                  label: 'Radio',
+                  index: 3,
+                ),
+                builtBottomNavBar(
+                  iconName: AppAssets.timeIcon,
+                  label: 'Time',
+                  index: 4,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  BottomNavigationBarItem builtBottomNavBar({
+    required String iconName,
+    required String label,
+    required int index,
+  }) {
+    return BottomNavigationBarItem(
+      icon:selectedIndex==index?
+      Container(
+        padding: EdgeInsets.symmetric(horizontal: 20,vertical: 6),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(66),
+          color: AppColor.blackbgColor
+        ),
+        child: ImageIcon(AssetImage(iconName)),
+      ):ImageIcon(AssetImage(iconName)) ,  label: label
+    );
   }
 }
--- lib/utils/images.dart
+++ lib/utils/images.dart
@@ -5,4 +5,11 @@ class AppAssets {
   static const String screen4 = 'assets/image/onboarding4.png';
   static const String screen5 = 'assets/image/onboarding5.png';
   static const String logo    = 'assets/image/islami_logo.png';
+  static const String quranBg= 'assets/image/Background.png' ;
+  static const String iconQuranBg= 'assets/image/quran_icon.png' ;
+  static const String hadethIcon= 'assets/image/hadeth_icon.png' ;
+  static const String sebhaIcon= 'assets/image/sebha_icon.png' ;
+  static const String radioIcon= 'assets/image/radio_icon.png' ;
+  static const String timeIcon= 'assets/image/time_icon.png' ;
+
 }
